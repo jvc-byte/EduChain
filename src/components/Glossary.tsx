20c8958cac7c49e0d11830f20c0c81dd5a4b5031
jvc <jvc8463@gmail.com>
2024-10-07 07:06:45 +0100

    update glossary

--- src/components/Glossary.tsx
+++ src/components/Glossary.tsx
@@ -99,6 +99,88 @@ const MyLessons = () => {
                     Each block contains an ordered list of transactions and consensus-related information. Blocks are proposed by validators, shared across the network,
                     and validated according to consensus rules.
                 </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Explorer</p>
+                <p className='text-2xl'>
+                    An interface that allows a user to search for information from, and about, a blockchain. This includes retrieving individual transactions, activity associated with specific addresses, and general information about the network.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Header</p>
+                <p className='text-2xl'>
+                    A collection of metadata about a block, summarizing its transactions in the execution payload. It includes essential information such as the previous block’s hash, timestamp, and Merkle root.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Header</p>
+                <p className='text-2xl'>
+                    A collection of metadata about a block, summarizing its transactions in the execution payload. It includes essential information such as the previous block’s hash, timestamp, and Merkle root.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Propagation</p>
+                <p className='text-2xl'>
+                    The process of transmitting a newly confirmed block to all other nodes across the network to maintain synchronization.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Proposer</p>
+                <p className='text-2xl'>
+                    The validator responsible for creating a block during a particular slot in the blockchain network. The selection of the proposer is often based on the consensus algorithm in use (e.g., proof-of-stake).
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Reward</p>
+                <p className='text-2xl'>
+                    The amount of cryptocurrency (e.g., ether) awarded to the proposer of a new, valid block as compensation for maintaining the network.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Status</p>
+                <p className='text-2xl'>
+                    A collection of metadata about a block, summarizing its transactions in the execution payload. It includes essential information such as the previous block’s hash, timestamp, and Merkle root.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Header</p>
+                <p className='text-2xl'>The states that a block can exist in. These include:</p><br />
+                <ul>
+                    <li className='text-2xl'><span className='font-bold'>- Proposed:</span> Block was proposed by a validator.</li>
+                    <li className='text-2xl'><span className='font-bold'>- Scheduled:</span> Validators are currently submitting data.</li>
+                    <li className='text-2xl'><span className="font-bold">- Missed/Skipped:</span> The proposer did not propose a block within the eligible time frame.</li>
+                    <li className='text-2xl'><span className="font-bold">- Orphaned:</span> The block was reorganized out by the fork choice algorithm.</li>
+                </ul>
+                <br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Time</p>
+                <p className='text-2xl'>
+                    The interval of time between two consecutive blocks being added to the blockchain. For example, in Ethereum, this is around 12 seconds.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Block Validation</p>
+                <p className='text-2xl'>
+                    The process of verifying that a new block contains valid transactions, signatures, and follows all the consensus rules. A valid block is added to the head of the chain and propagated across the network, while invalid blocks are disregarded.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Blockchain</p>
+                <p className='text-2xl'>
+                    A distributed database of transactions that is duplicated and shared across all computers in the network. Each block links to its predecessor, creating a secure chain of data. Blockchain’s integrity is cryptographically maintained through consensus mechanisms such as proof-of-stake.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Bootnode</p>
+                <p className='text-2xl'>
+                    A specialized node used during the node discovery process. It helps new nodes on the network find peers to connect with by introducing them to existing nodes.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Bridge</p>
+                <p className='text-2xl'>
+                    A blockchain bridge allows assets (like tokens) to be transferred from one blockchain network to another. For example, ETH can be transferred from Ethereum mainnet to Layer 2 scaling solutions using a bridge.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Bytecode</p>
+                <p className='text-2xl'>
+                    Code written in a compact, numeric format that is executed by the Ethereum Virtual Machine (EVM). It is the lower-level representation of smart contracts after compilation.
+                </p><br /><br /><br />
+
+                <p className='text-3xl pt-10 pb-5'>Byzantium Fork</p>
+                <p className='text-2xl'>
+                    A hard fork in Ethereum’s Metropolis development stage that included the delay of the Ice Age and a reduction in the block reward from 5 to 3 ether. This upgrade was part of the transition to proof-of-stake.
+                </p><br /><br /><br />
+
+
             </div>
             <Footer />
         </div>
