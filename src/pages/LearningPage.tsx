ac8c36cd1469876bfa9f0d14a65e4fa90d0d4e75
shittu121 <shittufaruq6@gmail.com>
2024-12-22 15:36:13 +0000

    fixed footer

--- src/pages/LearningPage.tsx
+++ src/pages/LearningPage.tsx
@@ -17,7 +17,7 @@ const LearningPage = () => {
          <button onClick={notify} className='border text-white mx-12 lg:mx-32 md:mx-32 my-10 hover:border-[#E6169B] p-1 w-28'>Load More</button>
       </div>
       <div className="text-white">
-         <h1 className='text-lg lg:text-2xl md:text-lg px-12 lg:px-32 md:px-32 py-5'>New On EduChain</h1>
+         <h1 className='text-lg lg:text-2xl md:text-lg px-12 lg:px-32 md:px-32 py-5'>New On Tea Protocol</h1>
          <NewCerticateCourse />
          <button onClick={notify} className='border text-white mx-12 lg:mx-32 md:mx-32 my-10 hover:border-[#E6169B] p-1 w-28'>Load More</button>
       </div>
